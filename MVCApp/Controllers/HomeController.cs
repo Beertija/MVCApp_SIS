cabfdd0ea5359b1b3b2d8436c8da92fbcfa72314
Tomislav Ogrinec <togrinec@foi.hr>
2022-01-06 23:38:19 +0100

    Added authorizations and changed forms display

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -1,24 +1,30 @@
 ﻿using MVCApp.Models;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using System.Web.Routing;
 using static DataLibrary.BusinessLogic.EmployeeProcessor;
 
 namespace MVCApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
+        [Authorize(Roles = "User, Admin")]
         public ActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
+        [Authorize(Roles = "User, Admin")]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
+        [AllowAnonymous]
+        [Authorize(Roles = "User, Admin")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -26,7 +32,7 @@ namespace MVCApp.Controllers
             return View();
         }
 
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         public ActionResult ViewEmployees()
         {
             ViewBag.Message = "Employees List";
@@ -48,6 +54,7 @@ namespace MVCApp.Controllers
             return View(employees);
         }
 
+        [Authorize(Roles = "User, Admin")]
         public ActionResult SignUp()
         {
             ViewBag.Message = "Employee Sign Up";
